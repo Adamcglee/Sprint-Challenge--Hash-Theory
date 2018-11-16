4764395a514e18a63d31d57a6ff787c14f89a3c4
adamcglee <adamcglee@gmail.com>
2018-11-16 10:45:40 -0800

    added code that doesn't work

--- hash-tables/ex1/ex1.c
+++ hash-tables/ex1/ex1.c
@@ -7,8 +7,27 @@ Answer *get_indices_of_item_weights(int *weights, int length, int limit)
 {
   HashTable *ht = create_hash_table(16);
 
-  // YOUR CODE HERE
-
+  for(int i = 0; i < length; i++) {
+    LinkedPair *pair = create_pair(i, weights[i]);
+    hash_table_insert(ht, pair->key, pair->value);
+  }
+  for(int j = 0; j < length; j++) {
+    char hashValue = hash_table_retrieve(ht, (limit - weights[j]));
+    if(hashValue != -1) {
+      for(int k = 0; k < length; k++) {
+        if(hashValue == weights[k])
+          answer->index_1 = k;
+      }
+      answer->index_2 = j;
+    }
+    int temp = 0;
+    if(answer->index_1 < answer->index_2) {
+      temp = answer->index_1;
+      answer->index_1 = answer->index_2;
+      answer->index_2 = temp;
+    }
+    return answer;
+  }
   return NULL;
 }
 
